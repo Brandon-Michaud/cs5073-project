e1908f9d9a36796e679c42f74ff0e1e6caa86893
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-22 15:12:07 -0500

    data preprocessing

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --partition=disc_dual_a100_students,debug,debug_5min
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=1G
+#SBATCH --output=outputs/job_%j_stdout.txt
+#SBATCH --error=outputs/job_%j_stderr.txt
+#SBATCH --time=00:05:00
+#SBATCH --job-name=test
+#SBATCH --mail-user=brandondmichaud@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/cs504319/cs5073-project
+
+. /home/fagg/tf_setup.sh
+conda activate anne
+
+python base.py -vv @cifar100.txt @oscer.txt --cpus_per_task $SLURM_CPUS_PER_TASK
