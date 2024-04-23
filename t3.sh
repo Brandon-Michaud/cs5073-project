489ed618302fb5f2a7a370b06bd249b8fed118e4
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-23 01:07:43 -0500

    all models test

---
+++ t3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --gres=gpu:1
+#SBATCH --partition=disc_dual_a100_students,gpu_a100,gpu
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=80G
+#SBATCH --output=outputs/job_%j_stdout.txt
+#SBATCH --error=outputs/job_%j_stderr.txt
+#SBATCH --time=06:00:00
+#SBATCH --job-name=t3
+#SBATCH --mail-user=brandondmichaud@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/cs504319/cs5073-project
+
+. /home/fagg/tf_setup.sh
+conda activate anne
+
+python base.py -vv @cifar100_transfer_resnet50.txt @oscer.txt --cpus_per_task $SLURM_CPUS_PER_TASK
